c1b23c0bd420421a796061a40971965776f6e2c0
Jamence <2232092982@qq.com>
2024-12-09 22:10:57 +0800

    add Latex解压

--- read_paper/deepseek.py
+++ read_paper/deepseek.py
@@ -2,6 +2,8 @@
 
 from openai import OpenAI
 
+
+ds_client = OpenAI(api_key="sk-4c723af969454bb790f889226a818c23", base_url="https://api.deepseek.com")
 ds_client = OpenAI(api_key="sk-4c723af969454bb790f889226a818c23", base_url="https://api.deepseek.com")
 
 # response = ds_client.chat.completions.create(
